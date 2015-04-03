74175fda87a559d975eaea9a2b19d10986b5d946
이주현 <aganga7427@naver.com>
2015-04-04 01:28:58 +0900

    커밋테스

--- ASDF/ASDF/소스.cpp
+++ ASDF/ASDF/소스.cpp
@@ -10,7 +10,7 @@ void commit()
 { 
 	printf("Hello World\n\n");
 	printf("14학번 이연규 입니다.");
-	printf("모두 영상보고 동기화 해주세요\n");ㄹㄹㄻㄴㄹㅇ
+	printf("모두 영상보고 동기화 해주세요\n");
 	printf("커밋 날려주세요\n");
 	
 	//Hit(힛) - 플레이어가 추가 카드를 더 원할 때 딜러에게 표현하는 용어.
@@ -162,7 +162,7 @@ void card_practice()
 
 
  /*이주현
- 카드돌리기.ㅔㅔㅔㅔㅔㅔ
+ 카드돌리기
 	 void main(){
 
 	 int a, b, c, d, f;
