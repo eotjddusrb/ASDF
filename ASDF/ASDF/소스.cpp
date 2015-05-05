ddec789586569b225229e2940a4ba3927710d4e9
eotjddusrb <eotjddusrb@naver.com>
2015-05-05 18:12:05 +0900

    ㅎㅎㅎㅎ

--- ASDF/ASDF/소스.cpp
+++ ASDF/ASDF/소스.cpp
@@ -7,7 +7,7 @@
 
 void shuffle();
 void QuickSort();
-void Devide(int pivot);
+
 
 //이제 정렬함수
 
@@ -60,7 +60,7 @@ void QuickSort()
 	int count = 0;
 	int pivot; 
 	
-	int pivotlist[20] = {};
+	int pivotlist[100] = {};
 
 
 	pivot = array[MAX-1]; // pivot 은 맨오른쪽 
@@ -170,22 +170,87 @@ void QuickSort()
 		}
 		
 	}
+	/////////////////////////////////////////////////
 	//여기까지 만듬& 확인
 	// 위의 과정으로 왼쪽부터 0 1 까지는 맞춰짐 ㅎ
 	
+
+
+
+
+
+	////////////////////////////////////////////////
+
+
 	//pivotlist 가 만들어짐 count로 갯수 셈
 
 	//우
+	count--;
+	pivot = pivotlist[count - 2];
+
+	while (pivot > 3)
+	{
+		size = pivot;
+		pivot = array[size - 1];
+		printf("\n\n\n");
+
+		pivotlist[count] = pivot;
+		count++;
+		printf("\n\n %d번째 pivot = %d \n\n", count, pivot);
+
+		i = 0;
+		j = size - 2;
+
+		while (1) // 좌측 조지기
+		{
 
 
+			if (array[i] > pivot && array[j] < pivot)
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+			}
 
+			if (array[i] < pivot)
+			{
+				i++;
+			}
+
+			if (i >= pivot)
+			{
+				break;
+			}
+
+			if (array[j] > pivot)
+			{
+				j--;
+			}
+		}
+
+		temp = array[pivot];
+		array[pivot] = array[size - 1];
+		array[size - 1] = temp;
+
+		printf("\n\n\n");
+		printf("size = %d\n", size);
+		printf("array[size] = %d \n", array[size]);
+		printf("pivot = %d \n", pivot);
+		printf("array[pivot] = %d \n", array[pivot]);
+		printf("\n\n\n");
+
+		for (i = 0; i < pivot + 1; i++)
+		{
+			printf("%d ", array[i]); //배열확인
+		}
+	}
 
 	/*
 	while (count)
 	{
 		count--;
 		
-		pivot = array[pivotlist[count - 1]-1]; //지난 pivot-1자리가 새로운 pivot
+		pivot = array[pivotlist[count - 2]-1]; //지난 pivot-1자리가 새로운 pivot
 
 		i = pivotlist[count]+1; //바로 전 pivot 다음 부터 정렬
 		j = pivot - 1; // pivot앞까지 정렬
@@ -222,9 +287,9 @@ void QuickSort()
 		for (i = 0; i < pivot + 1; i++)
 			printf("%d ", array[i]);
 
-
-	}
-	*/ 
+		break;
+	}*/
+	 
 	// 이거 심각하네 ㅜㅜ
 
 
@@ -237,95 +302,6 @@ void QuickSort()
 	for (i = 0; i < MAX; i++)
 	{
 		printf("%d ", array[i]);
-	}
-
 	
-
-
-
-
-
-
-
-
-
-
-
-	//Devide(left); // 배열을 넣을 때 이름만 넣는다.
-	
-	/*
-	//확인차 출력
-	printf("\n\n\n");
-	for (i = 0; i < MAX; i++)
-	{
-		printf("%d ", left[i]);
-	}
-
-	printf("\n\n %d \n\n", pivot);
-
-	for (i = 0; i < MAX; i++)
-	{
-		printf("%d ", right[i]);
-	}
-	*/
 
 }
-/*
-void Devide(int size)
-{
-	int i, j, temp;
-	int pivot;
-	int* left;
-
-	left = (int*)malloc(size*sizeof(int));
-	
-	printf("\n\n 디바이드 들어와쩌염\n\n\n");
-	for (i = 0; i < size; i++)
-	{
-		left[i] = left[i];
-		printf(" %d", left[i]);
-	}
-
-	printf("\n\n\n");
-
-	pivot = left[size-1];
-
-	i = 0;
-	j = size - 2;
-
-	while (1) 
-	{
-		if (left[i] > pivot && left[j] < pivot)
-		{
-			temp = left[i];
-			left[i] = left[j];
-			left[j] = temp;
-			printf("swap ");
-		}
-
-		if (left[i] < pivot)
-		{
-			printf("i++ ");
-			i++;
-		}
-
-		if (i >= pivot)
-		{
-			break;
-		}
-
-		if (left[j] > pivot)
-		{
-			j--;
-			printf("j-- ");
-		}
-	}
-
-	temp = left[pivot];
-	left[pivot] = left[MAX - 1];
-	left[MAX - 1] = temp;
-
-
-	
-	free(left);
-}*/
